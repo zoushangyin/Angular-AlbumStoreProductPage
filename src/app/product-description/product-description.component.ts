b1d877e6705eec274c1eb65110af53ad283be456
shangyin.zou <zoushangyin@gmail.com>
2022-06-20 14:51:47 +0900

    module 3 fix

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ProductService } from 'app/product.service';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-description',
