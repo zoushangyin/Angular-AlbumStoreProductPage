e2e5744756ffaa7d1b4a1fc9ccbb1ecfb464f13b
shangyin.zou <zoushangyin@gmail.com>
2022-06-20 15:49:47 +0900

    module 6

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -5,11 +5,13 @@ import { Http, Response } from '@angular/http';
 import "rxjs/add/operator/map";
 import { Album } from './album';
 import { Observable } from 'rxjs/Observable';
+import { Product } from './product';
 
 @Injectable()
 export class ProductService {
 
   private _albumUrl = "../assets/album.json";
+  private _productUrl = "../assets/album.json";
 
   constructor(private _http: Http) { 
     
@@ -19,6 +21,10 @@ export class ProductService {
     return this._http.get(this._albumUrl).map(
       (response) => <Album>response.json()
     );
+  }
+
+  getProducts() : Observable<Product[]>{
+    return this._http.get(this._productUrl).map(response => <Product[]>response.json());
   }
 
 }
