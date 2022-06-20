9253a1b01a78581d83f6e75786feca06373fb3ae
shangyin.zou <zoushangyin@gmail.com>
2022-06-20 16:21:19 +0900

    module 7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,14 @@ import { ProductDescriptionComponent } from './product-description/product-descr
 import { ProductService } from './product.service';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes : Routes = [
+  { path: "products", component: ProductListComponent},
+  { path: "product/:id", component: ProductPageComponent},
+  { path: "", redirectTo: "products", pathMatch: "full" }
+
+];
 
 @NgModule({
   declarations: [
@@ -21,6 +29,7 @@ import { ProductListComponent } from './product-list/product-list.component';
   imports: [
     BrowserModule,
     FormsModule,
+    RouterModule.forRoot(appRoutes),
     HttpModule
   ],
   providers: [ProductService],
