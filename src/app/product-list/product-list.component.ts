db5a0b5791c12e7db67645e824a754788d9d266f
shangyin.zou <zoushangyin@gmail.com>
2022-06-20 16:05:49 +0900

    module 6  fix 3

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,5 +1,5 @@
-import { Component, Injectable, OnInit } from '@angular/core';
-import { Product } from "../product";
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../product';
 import { ProductService } from '../product.service';
 
 @Component({
@@ -7,13 +7,11 @@ import { ProductService } from '../product.service';
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-
 export class ProductListComponent implements OnInit {
 
-  products : Product[];
-  private _productUrl : string = "../assets/products.json";
+  products: Product[];
 
-  constructor(private _productService : ProductService) { }
+  constructor(private _productService: ProductService) { }
 
   ngOnInit() {
     this._productService.getProducts().subscribe(response => this.products = response);
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,30 +1,27 @@
-import { getLocaleNumberSymbol } from '@angular/common';
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
-//import { Observable } from 'rxjs';
-import "rxjs/add/operator/map";
+
+import 'rxjs/add/operator/map';
+
 import { Album } from './album';
+
 import { Observable } from 'rxjs/Observable';
+
 import { Product } from './product';
 
 @Injectable()
 export class ProductService {
 
-  private _albumUrl = "../assets/album.json";
-  private _productUrl = "../assets/products.json";
+  private _albumUrl = '../assets/album.json';
+  private _productsUrl = '../assets/products.json';
 
-  constructor(private _http: Http) { 
-    
+  constructor(private _http: Http) { }
+
+  getAlbum(id: number): Observable<Album> {
+    return this._http.get(this._albumUrl).map((response) => <Album>response.json());
   }
 
-  getAlbum  (id : number) : Observable<Album> {
-    return this._http.get(this._albumUrl).map(
-      (response) => <Album>response.json()
-    );
+  getProducts(): Observable<Product[]> {
+    return this._http.get(this._productsUrl).map((response) => <Product[]>response.json());
   }
-
-  getProducts() : Observable<Product[]>{
-    return this._http.get(this._productUrl).map((response) => <Product[]>response.json());
-  }
-
 }
